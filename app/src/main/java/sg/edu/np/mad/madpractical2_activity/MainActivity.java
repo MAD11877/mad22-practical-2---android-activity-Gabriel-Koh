66d37c911d626ed74243c7675623d8bef42df65b
Gabriel-Koh <94812128+Gabriel-Koh@users.noreply.github.com>
2022-05-16 17:28:17 +0800

    Project file

---
+++ app/src/main/java/sg/edu/np/mad/madpractical2_activity/MainActivity.java
@@ -0,0 +1,21 @@
+package sg.edu.np.mad.madpractical2_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+public class User {
+    String name;
+
+
+    }
+}
